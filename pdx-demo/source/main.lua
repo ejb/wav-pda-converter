b31d83f232740a904a9bd3d0e6b234bb4ed21773
ejb <mail@elliotbentley.com>
2025-04-06 16:58:09 +0100

    Add PDX for testing

---
+++ pdx-demo/source/main.lua
@@ -0,0 +1,16 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local audio_file = "./Assets/SetPuzzleComplete-Mono-IMAADPCM"
+print('playing '..audio_file)
+local mySound, error = playdate.sound.sampleplayer.new(audio_file)
+mySound:play(0)
+
+function playdate.update()
+  playdate.graphics.drawTextInRect(audio_file,10,10, 360, 400)
+  if error then
+    playdate.graphics.drawTextInRect(error,10,60, 360, 400)
+  end
+end
